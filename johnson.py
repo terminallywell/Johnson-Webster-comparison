bd538b3ee8a1c273c97829f4fc4246d42726b28e
terminallywell <hwanjuncho@umass.edu>
2024-09-01 17:06:19 -0400

    merging multiple xmls

--- johnson.py
+++ johnson.py
@@ -5,12 +5,24 @@ import xml.etree.ElementTree as ET
 words = [filename.split('-')[1] for filename in os.listdir('XMLs')]
 
 
-def getheadword(filepath: str) -> str:
-    return filepath.split('-')[1]
+# need to account for redirects
 
-def getxml(word: str) -> ET.Element: # TODO: merge multiple files for one word
-    with open(f'XMLs/f1755-{word}-1.xml') as file:
-        return ET.parse(file).getroot()
+# TODO: spelling (and hyphenation?) differences (e.g. offense/offence), any redirects?
+
+def getxmls(word: str) -> list[ET.Element]:
+    xmls = []
+    
+    n = 1
+    while True:
+        try:
+            with open(f'XMLs/f1755-{word}-{n}.xml') as file:
+                xmls.append(ET.parse(file).getroot())
+                n += 1
+        except FileNotFoundError:
+            break
+
+    return xmls
+
 
 def extract_text(element: ET.Element) -> str:
     text = element.text
@@ -19,11 +31,15 @@ def extract_text(element: ET.Element) -> str:
         text += child.tail # type: ignore
     return text.strip() # type: ignore
 
-def getdefs(element: ET.Element) -> list[str]:
-    return list(map(extract_text, element.findall('.//tei:def', {'tei': 'http://www.tei-c.org/ns/1.0'})))
 
+def getdefs(word: str) -> list[str]:
+    defs = []
+    
+    for element in getxmls(word):
+        for raw in element.findall('.//tei:def', {'tei': 'http://www.tei-c.org/ns/1.0'}):
+            defs.append(extract_text(raw))
 
-for d in getdefs(getxml('weapon')): # need to account for redirects
+    return defs
+
+for d in getdefs('match'):
     print(d + '\n')
-
-# TODO: spelling and hyphenation differences (offense/offence)
