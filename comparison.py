3405d76c4646d77bd78c0e34f0d70f39391d4d94
terminallywell <hwanjuncho@umass.edu>
2025-02-09 00:17:40 -0500

    separate preprocessing, add short entry trim

--- comparison.py
+++ comparison.py
@@ -1,56 +1,97 @@
-# checking which headwords in Johnson is missing in NWAD
-
-with open('words.txt', encoding='utf8') as file:
-    johnson = set(file.read().split())
-
-with open('NWAD_words.txt', encoding='utf8') as file:
-    nwad = set(file.read().lower().replace('-', '').split()) # remove hyphen in headwords
-
-missing = [word for word in johnson if not word in nwad]
-
-with open('missing.txt', 'w') as file:
-    file.write('\n'.join(sorted(missing)))
-
-
-# Spelling differences (Webster/Johnson)
-# * -or/-our (colour, ardour, labour, ...) search: our\n
-# * -ic/-ick (academick, tactick, ...) search: ick\n
-# * -er/-re (fibre, centre, lustre, ...) search: [^aeioucr]re\n
-# * -ise/-ize (modernise, ...)
-# * offense/offence
-# * connection/connexion
-# * willful/wilful, skillful/skilful
-# * spectacle/specktacle
-# * ax/axe, pickax/pickaxe
-# * skirr/skirre
-# * ransom/ransome
-# * racoon/rackoon (raccoon)
-
-# Hyphenation (remove from NWAD)
-# * afternoon/after-noon
-
-# apply changes and make new list
 import re
+import json
+from johnson import getdefs
+from preprocess import tokenize
+from difflib import SequenceMatcher
 
-johnson_new = []
-for word in johnson:
-    word = re.sub(r'our$', 'or', word)
-    word = re.sub(r'ick$', 'ic', word)
-    word = re.sub(r'([^aeioucr])re$', r'\1er', word)
-    word = re.sub(r'ise$', r'ize', word)
-    johnson_new.append(word)
-
-# TODO: make dict of spelling changes e.g. {'colour': 'color'} for tabulating purposes
-
-nwad_new = [word.replace('-', '') for word in nwad]
-
-missing = [word for word in johnson_new if not word in nwad_new]
-
-with open('missing_new.txt', 'w') as file:
-    file.write('\n'.join(sorted(missing)))
-
-
-# find matches
 ## tokenization:
 ## remove articles
 ## capture 2+ subseq (as opposed to 3)
+
+
+nwad = json.load(open('nwad.json'))
+# remove POS entries from nwad
+for word in nwad:
+    old = nwad[word]
+    new = [d for d in old if len(d) > 5]
+    nwad[word] = new
+
+
+junk = {'(the)', 'of', 'to', 'or', 'and', 'in', 'by', 'that', 'as', 'for', 'from'}
+def nonjunklen(seq):
+    return sum(1 for word in seq if word not in junk)
+
+
+matches = []
+for word in nwad:
+    for sense_nw in nwad[word]:
+        tm = tokenize(sense_nw)
+        for sense_johnson in getdefs(word):
+            tn = tokenize(sense_johnson)
+            sm = SequenceMatcher(None, tm , tn).get_matching_blocks()
+            blocks = [tm[a:a+size] for a, b, size in sm if nonjunklen(tm[a:a+size]) > 0] # minimum block length = 1
+            if (matchlen := sum(map(len, blocks))) > 2: # minimum total length = 3
+                matches.append((
+                    word,
+                    sense_nw,
+                    sense_johnson,
+                    ' [...] '.join(' '.join(block) for block in blocks),
+                    matchlen,
+                    max(map(len, blocks))
+                    ))
+
+for i, t in enumerate(sorted(matches, key = lambda t: t[5])):
+    print(f'{i+1}. {t[0]}: ... {t[3]} ...')
+
+# match format: word/M-W sense/1812 sense/matching subsequence/total match length/longest contiguous match length
+with open('matches.tsv', 'w', encoding='utf8') as file:
+    file.write('word\tM-W sense\t1812 sense\tmatching subsequence\ttotal match length\tlongest contiguous match length\n')
+    for match in matches:
+        file.write('\t'.join(map(str, match)) + '\n')
+
+
+
+from rapidfuzz import fuzz, utils, distance
+
+fuzz.ratio('aft', 'after')
+
+distance.DamerauLevenshtein.distance('connexion', 'connection')
+
+a1 = 'definition string with lovesick colour difference willfully ignorant.'
+a2 = 'this is a dummy string trying to throw off the fuzz definition string lovesic some color difference wilfully ignorant. '
+
+a1 = 'what if i added even more one matching string this is long this is a dummy string; followed by'
+a2 = 'one matching string this is long you are not supposed to be the same'
+
+fuzz.partial_ratio(a1, a2, processor=utils.default_process, score_cutoff=30)
+
+matches = []
+for word in nwad:
+    for sense_nw in nwad[word]:
+        if len(sense_nw) < 4:
+            continue
+        for sense_johnson in getdefs(word):
+            ratio = fuzz.partial_ratio(
+                sense_nw,
+                sense_johnson,
+                processor=utils.default_process,
+                score_cutoff=30
+            )
+            if ratio:
+                matches.append(
+                    (
+                        word,
+                        sense_nw,
+                        sense_johnson,
+                        ratio
+                    )
+                )
+
+for i, t in enumerate(sorted(matches, key = lambda t: t[3])):
+    print('\t'.join(map(str, t)))
+
+# match format: word/M-W sense/1812 sense/matching subsequence/total match length/longest contiguous match length
+with open('matches.tsv', 'w', encoding='utf8') as file:
+    file.write('word\tM-W sense\t1812 sense\tmatching subsequence\ttotal match length\tlongest contiguous match length\n')
+    for match in matches:
+        file.write('\t'.join(map(str, match)) + '\n')
